64fd3c196cdd2a474555353b60a7e895709d5e34
adeiskandarzulkarnaen <adeiskandarzulkarnaen@gmail.com>
2024-11-28 10:22:12 +0700

    add config file

---
+++ src/Commons/config.ts
@@ -0,0 +1,15 @@
+
+const config = {
+  app: {
+    port: process.env.PORT,
+  },
+  database: {
+    url: process.env.DATABASE_URL,
+  },
+  jwt: {
+    age: process.env.ACCESS_TOKEN_AGE,
+    secret: process.env.ACCESS_TOKEN_SECRET,
+  }
+};
+
+export default config;
