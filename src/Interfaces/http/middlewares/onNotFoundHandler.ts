bae1c5f0548bb024d742ca82f62c3e3f60ede4c7
adeiskandarzulkarnaen <adeiskandarzulkarnaen@gmail.com>
2024-12-01 15:40:31 +0700

    update onError middleware

--- src/Interfaces/http/middlewares/onNotFoundHandler.ts
+++ src/Interfaces/http/middlewares/onNotFoundHandler.ts
@@ -3,7 +3,7 @@ import { Context } from 'hono';
 function onNotFoundHandler(c: Context) {
   return c.json({
     status: 'fail',
-    message: 'endpoint not found',
+    message: 'the requested resource does not exist', // 'endpoint not found'
   }, 404);
 };
 
